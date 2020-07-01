11164b3e5f515da2cbbeef7cf633f2ad6207a3d8
dwklingeman <dwklingeman1s@semo.edu>
2020-07-01 15:08:15 -0500

    Finished Part 1

---
+++ hw02/hw02-1.R
@@ -0,0 +1,136 @@
+# Dylan Klingeman
+
+### HW02 Part 1: Intro to Basics
+
+# Calculations: 30+12
+30+12
+#answer should be 42
+
+### 1.2 Arithmetic with R
+
+# division: 121/.1
+121/1.1
+#answer should be 11~
+
+# Exponation: 2^5
+2^5
+# answer should be 32
+
+# Modulo: 28%%6
+28 %% 6
+#a nswer should be 4
+
+# Exponation without parantheses: 49^1/2
+49^1/2
+# answer should be 24.5
+
+# Exponation with parantheses: 49^(1/2)
+49^(1/2)
+# answer should be 7
+
+### 1.3 Variable assignment
+
+#Assign the value 42 to x
+my_var <- 42
+
+# Print out the value of the variable x
+my_var
+
+#
+
+# Assign the value 24 to the variable d_merriami
+d_merriami <- 24
+
+# Assign the value 9 to d_ordii
+d_ordii <- 9
+
+# Check the results
+d_merriami
+d_ordii
+
+# Answer should be 1,24 and 1, 9
+
+### 1.4 Working with Variables
+
+# Correcting the mistake: d_merriami + 5
+d_merriami + 5
+
+d_merriami
+
+# d_merriami should NOT change from 24->29
+
+# Correcting the mistake 
+d_merriami <- d_merriami + 5
+d_merriami
+
+# D_merriami should change from 24->29
+
+ x <- 30
+ y <- 12
+ z <- x + y
+ z
+ 
+ # z should equal 42
+
+ # Add Together the number of individuals of each species. 
+ # Store the results in `total_k_rats`.
+ total_k_rats <- d_merriami + d_ordii
+ total_k_rats
+ # Total should be 38
+ 
+ # Assign 1000 to a variable called `area sampled`
+ area_sampled <- 1000
+ area_sampled
+ 
+ #Divide `total_k_rats` by `area_sampled` to calculate `density`
+ density <- total_k_rats / area_sampled
+ 
+ 
+ # Density should be 0.038
+ 
+ #Show the value stored in density
+ density
+ 
+ 
+ #
+ 
+ ### 1.5 Basic data types in R
+ 
+ # Assign 42 to numeric_var
+ numeric_var <- 42
+ numeric_var
+ 
+ # numeric_var should be 42
+ 
+ # Assign "Dipodomys merriami" to species_name
+ species_name <- "dipodomys merriami"
+ species_name
+ 
+ #species_name should answer with "dipodomys merriami"
+ 
+ # Assign true to is_mammal (Remember that Case matters)
+ is_mammal <- TRUE
+ is_mammal
+ 
+ # is_mammal should be TRUE
+ 
+ ### 1.6 What is the data type? 
+ 
+ # Check class of numeric_var
+ class(numeric_var)
+ 
+ #numeric_var should be numeric
+ 
+ # Check class of species_name
+ class(species_name)
+ 
+ # species_name should be character
+ 
+ # Check class of is_mammal
+ class(is_mammal)
+ 
+ # is_mammal should be logical
+ 
+ #
+ 
+ 
