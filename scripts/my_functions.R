8a340dd817aac75132d1f88e5bd94e38eed3a7af
dwklingeman <dwklingeman1s@semo.edu>
2020-07-21 20:53:00 -0500

    Both functions done

--- scripts/my_functions.R
+++ scripts/my_functions.R
@@ -15,15 +15,18 @@ std_err <- function(x, na.rm =FALSE) {
 # The if else statement will by default will keep NA values. However, if "na.rm = TRUE" is placed in a separate argument, it will remove NA values. 
 # The formula for standard error of the mean. sd = standard deviation
 
-fib_sequence <- c(0, NA, 1, 1, 2, 3, 5, 8, 13, 21, 34)
+#
 
+## Scaled mass index function (Working)
 
-## Scaled mass index function (Not working)
-
-scaled_mass <- (mass * ((mean (tarsus) /tarsus) ^ slope))
+scaled_mass <- function(mass=0, tarsus=0, slope=0) {
+  mass*((mean(tarsus)/tarsus)^slope)
+}
 
 # Creates the argument for scaled_mass(). This is used to scale the mass of a bird to the length of it's tarsus. 
-
+# The defaults are set to 0 for: mass, tarsus, and slope. 
+# The formula for the scaled mass index.
+# 
 mass <- c(26.7, 22.8, 24.7, 26.1, 23.9)
 
 # mass values for scaled_mass() argument
@@ -35,3 +38,5 @@ tarsus <- c(18.4, 17.5, 18.4, 18.0, 18.2)
 slope <- (1.5)
 
 # slope value for scaled_mass argument
+
+#
